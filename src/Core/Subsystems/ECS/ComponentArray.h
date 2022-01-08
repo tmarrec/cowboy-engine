dc136ee267d0ea270f624f00f3a9d84ab328b53b
trietch <trietch@gmail.com>
2022-01-08 18:51:08 +0100

    Windows compatibility

--- src/Core/Subsystems/ECS/ComponentArray.h
+++ src/Core/Subsystems/ECS/ComponentArray.h
@@ -5,6 +5,7 @@
 #include <cassert>
 #include <limits>
 #include <unordered_map>
+#include <array>
 
 class IComponentArray
 {
--- src/Core/Subsystems/Renderer/Renderer.h
+++ src/Core/Subsystems/Renderer/Renderer.h
@@ -14,6 +14,7 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <chrono>
 #include <unordered_map>
+#include <numbers>
 
 #include "world/World.h"
 #include "Shader.h"
